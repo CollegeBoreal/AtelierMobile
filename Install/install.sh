831322e6a352cf8dda0b296bb0ded28efff499d3
Setra <setrar@valiha.com>
2018-05-04 18:33:58 -0400

    new version

--- Install/install.sh
+++ Install/install.sh
@@ -33,16 +33,21 @@ echo "export JAVA_HOME=$(/usr/libexec/java_home)" >> ~/.bash_profile
 #2. Installer Android SDK
 
 #   * Installer le SDK par le terminal  
-brew install android-sdk
+brew cask install android-sdk
 
 #   * Initialiser Android  
 echo "export ANDROID_HOME=/usr/local/opt/android-sdk" >> ~/.bash_profile
 
 #   * Mettre à jour les API 23   
-echo y | android update sdk --filter tools,platform-tools,android-23,build-tools-23.0.3,extra-android-m2repository,extra-google-m2repository,extra-android-support --all --no-ui 
+sdkmanager --install "build-tools;23.0.1" "platform-tools" "platforms;android-23"
 
 #   * Mettre à jour les Images Systèmes 23
-echo y | android update sdk --filter sys-img-x86_64-android-23 --all --no-ui 
+echo y | sdkmanager "system-images;android-23;default;x86_64"
 
 #   * Configurer un appareil android
-echo no | android create avd --name Nexus_6 --target android-23 --skin WVGA800 --abi x86_64 --path ~/.android/avd/Nexus_6
+avdmanager create avd -n Nexus_6 -k "system-images;android-23;default;x86_64" --abi default/x86_64
+
+
+# * Installer l'emulateur
+
+echo "alias emulator='$ANDROID_HOME/tools/emulator'" >> ~/.bash_profile
